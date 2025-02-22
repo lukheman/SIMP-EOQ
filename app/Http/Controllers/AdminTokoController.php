30b6f46a9ca8587d3f62cedb965724abac17144d
Akmal <akmalthemalong@gmail.com>
2025-02-22 16:55:03 +0800

    feat(admintoko): menambahkan handler untuk mencetak nota pesanan

--- app/Http/Controllers/AdminTokoController.php
+++ app/Http/Controllers/AdminTokoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaksi;
+use App\Helpers\QrcodeHelper;
 
 class AdminTokoController extends Controller
 {
@@ -19,4 +21,16 @@ class AdminTokoController extends Controller
         ]);
     }
 
+    public function nota(Request $request) {
+
+        $request->validate([
+            'id_transaksi' => 'required|exists:transaksi,id'
+        ]);
+
+        $qrcode = QrcodeHelper::getQrcodeString($request->id_transaksi);
+
+        return view('invoices.nota', [ 
+            'qrcode' => $qrcode
+        ]);
+    }
 }
