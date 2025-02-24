a4ba2004162d4a581ea72fe86e7fc28661ba52f3
Akmal <akmalthemalong@gmail.com>
2025-02-24 17:10:45 +0800

    feat(api): tambah method show

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -62,4 +62,24 @@ class TransaksiController extends Controller
 
     }
 
+    public function show($id) {
+
+        $transaksi = Transaksi::with(['produk', 'user', 'user.reseller_detail'])->findOrFail($id);
+
+        if($transaksi) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Berhasil mendapatkan data transaksi',
+                'data' => $transaksi
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Gagal mendapatkan data transaksi'
+        ], 500);
+
+
+    }
+
 }
