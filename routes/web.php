9cd76aa733da403f7f6d87d1a35318c13c7c842c
Akmal <akmalthemalong@gmail.com>
2025-02-24 17:20:09 +0800

    new routes

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,25 @@ use App\Http\Controllers\ResellerController;
 use App\Http\Controllers\PemilikTokoController;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\PesananController;
 
 use Illuminate\Support\Facades\Route;
+
+Route::controller(ResellerController::class)->group(function() {
+    Route::get('reseller', 'index')->name('reseller.index');
+    Route::get('reseller/dashboard', 'index')->name('reseller.dashboard');
+    Route::get('reseller/katalog', 'katalog')->name('reseller.katalog');
+    Route::get('reseller/keranjang', 'keranjang')->name('reseller.keranjang');
+    Route::get('reseller/pesanan', 'pesanan')->name('reseller.pesanan');
+    Route::get('reseller/pengiriman', 'pengiriman')->name('reseller.pengiriman');
+
+});
 
 Route::controller(AdminTokoController::class)->group(function() {
     Route::get('admintoko', 'index')->name('admintoko.index');
     Route::get('admintoko/dashboard', 'index')->name('admintoko.dashboard');
     Route::get('admintoko/pesanan', 'pesanan')->name('admintoko.pesanan');
+    Route::get('admintoko/persediaan', 'persediaan')->name('admintoko.persediaan');
     Route::post('admintoko/nota', 'nota')->name('admintoko.nota');
 });
 
@@ -30,13 +42,6 @@ Route::controller(AdminGudangController::class)->group(function() {
     Route::get('admingudang/pesanan', 'pesanan')->name('admingudang.pesanan');
 });
 
-Route::controller(ResellerController::class)->group(function() {
-    Route::get('reseller', 'index')->name('reseller.index');
-    Route::get('reseller/dashboard', 'index')->name('reseller.dashboard');
-    Route::get('reseller/katalog', 'katalog')->name('reseller.katalog');
-    Route::get('reseller/pesanan', 'pesanan')->name('reseller.pesanan');
-    Route::get('reseller/pengiriman', 'pengiriman')->name('reseller.pengiriman');
-});
 
 Route::controller(PemilikTokoController::class)->group(function() {
     Route::get('pemiliktoko', 'index')->name('pemiliktoko.index');
@@ -46,17 +51,27 @@ Route::controller(PemilikTokoController::class)->group(function() {
 Route::controller(KurirController::class)->group(function() {
     Route::get('kurir', 'index')->name('kurir.index');
     Route::get('kurir/dashboard', 'index')->name('kurir.dashboard');
-    Route::get('kurir/konfirmasiPembayaran', 'konfirmasiPembayaran')->name('kurir.konfirmasi-pembayaran');
+    Route::get('kurir/pesanan', 'pesanan')->name('kurir.pesanan');
+    Route::get('kurir/konfirmasiPembayaran', 'scanQrcode')->name('kurir.scan-qrcode');
+    Route::post('kurir/konfirmasiPembayaran', 'konfirmasiPembayaran')->name('kurir.konfirmasi-pembayaran');
 });
 
 Route::controller(ProdukController::class)->group(function() {
     Route::get('produk/all', 'all')->name('produk.all');
 });
+
 Route::resource('produk', ProdukController::class)->only(['store', 'update', 'destroy', 'show']);
+Route::resource('pesanan', PesananController::class)->only(['store', 'update', 'destroy', 'show']);
+Route::controller(PesananController::class)->group(function() {
+    Route::post('pesanan/checkout', 'checkout')->name('pesanan.checkout');
+});
 
 Route::resource('persediaan', PersediaanController::class)->only(['store', 'update', 'destroy', 'show']);
 
 Route::resource('transaksi', TransaksiController::class)->only(['store', 'update', 'destroy', 'show']);
+Route::controller(TransaksiController::class)->group(function() {
+    Route::post('transaksi/detail', 'detail')->name('transaksi.detail');
+});
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
