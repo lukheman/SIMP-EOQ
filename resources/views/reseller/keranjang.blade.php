924c32e03f73d6bcea5e4c21d72e4026dbdc4343
Akmal <akmalthemalong@gmail.com>
2025-02-24 17:19:28 +0800

    feat(view): keranjang.blade.php

---
+++ resources/views/reseller/keranjang.blade.php
@@ -0,0 +1,142 @@
+@extends('layouts.main')
+
+@section('title', 'Toko Kecil')
+
+@section('sidebar-menu')
+@include('reseller.menu')
+@endsection
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <button type="button" class="btn btn-outline-primary" id="btn-checkout">Checkout</button>
+
+    </div>
+    <div class="card-body">
+        <div id="table_pesanan_wrapper" class="dataTables_wrapper dt-bootstrap4">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+
+                </div>
+                <div class="col-sm-12 col-md-6">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <!-- TODO: buat menjadi lebih estetik -->
+                    <table id="table_pesanan" class="table table-bordered table-striped dataTable dtr-inline"
+                        aria-describedby="table_pesanan_info">
+                        <thead>
+                            <tr>
+                                <th class="sorting" tabindex="0" aria-controls="table_pesanan" rowspan="1" colspan="1">
+                                    Nama Produk</th>
+                                <th class="sorting" tabindex="0" aria-controls="table_pesanan" rowspan="1" colspan="1">
+                                    Jumlah
+                                </th>
+                                <th class="sorting" tabindex="0" aria-controls="table_pesanan" rowspan="1" colspan="1">
+                                    Harga Satuan (Rp.)
+                                </th>
+
+                                <th class="sorting" tabindex="0" aria-controls="table_pesanan" rowspan="1" colspan="1">
+                                    Total Harga (Rp.)
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach ($pesanan as $item)
+                            <tr>
+                                <td> {{ $item->produk->nama_produk }}</td>
+                                <td> {{ $item->jumlah }}</td>
+                                <td> {{ number_format($item->produk->harga_jual, 0, ',', '.')}}</td>
+                                <td> {{ number_format($item->total_harga, 0, ',', '.')}}</td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                        <tfoot>
+
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-5">
+                </div>
+                <div class="col-sm-12 col-md-7">
+                    <div class="dataTables_paginate paging_simple_numbers" id="table_pesanan_paginate">
+                        <ul class="pagination">
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('custom-script')
+
+<script>
+
+    $(document).ready(() => {
+
+        // TODO: beri peringatan bahwa pemesanan tidak bisa dibatalkan
+        $('#btn-checkout').click(function () {
+
+            Swal.fire({
+                title: "Checkout semua barang di keranjang?",
+                text: "Tindakan tidak dapat dibatalkan.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, checkout"
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    $.ajax({
+                        url: '{{ route('pesanan.checkout')}}',
+                        method: 'POST',
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        success: function (data) {
+                            Swal.fire({
+                                title: "Berhasil!",
+                                text: data.message,
+                                icon: "success"
+                            }).then(() => window.location.reload());
+                        },
+                        error: function (error) {
+                            console.log(error);
+                        }
+                    });
+
+
+                }
+            });
+        });
+
+    });
+
+</script>
+
+<script>
+    $(function () {
+
+        $('#table_pesanan').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+
+    });
+</script>
+
+@endsection
