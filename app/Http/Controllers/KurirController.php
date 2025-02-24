ca551c7dc08765265da8f2d5a27c926d3b747c60
Akmal <akmalthemalong@gmail.com>
2025-02-24 17:15:48 +0800

    feat: KurirController.php

--- app/Http/Controllers/KurirController.php
+++ app/Http/Controllers/KurirController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaksi;
 
 class KurirController extends Controller
 {
-    //
+
+    public function pesanan() {
+        $pesanan = Transaksi::with('produk')->with('user')->get();
+
+        // dd($pesanan);
+
+        return view('kurir.pesanan', [
+            'page' => 'Pesanan',
+            'pesanan' => $pesanan
+        ]);
+    }
+
+    public function scanQrcode() {
+        return view('kurir.scan-qrcode', [
+            'page' => 'Konfirmasi Pembayaran'
+        ]);
+    }
+
+    public function konfirmasiPembayaran(Request $request) {
+        $request->validate([
+            'id_transaksi' => 'required|exists:transaksi,id'
+        ]);
+
+        $transaksi = Transaksi::with(['produk', 'user', 'user.reseller_detail'])->findOrFail($request->id_transaksi);
+
+        if($transaksi->status === 'dibayar') {
+            return response()->json([
+                'success' => true,
+                'message' => 'Pembayaran telah dikonfirmasi pada ' . $transaksi->updated_at,
+                'data' => $transaksi
+            ]);
+        }
+
+        $transaksi->status = 'dibayar';
+        $transaksi->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Pembayaran berhasil dikonfimasi',
+            'data' => $transaksi
+        ], 200);
+
+
+    }
+
 }
