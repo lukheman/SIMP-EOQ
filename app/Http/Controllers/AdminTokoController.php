53795382d26e6c8caa4329fd0fd952c80a6bacd5
Akmal <akmalthemalong@gmail.com>
2025-03-01 11:14:49 +0800

    feat: tambah handler untuk nota

--- app/Http/Controllers/AdminTokoController.php
+++ app/Http/Controllers/AdminTokoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Penjualan;
+use App\Models\Pesanan;
 use Illuminate\Http\Request;
 use App\Models\Transaksi;
 use App\Helpers\QrcodeHelper;
@@ -38,10 +40,18 @@ class AdminTokoController extends Controller
             'id_transaksi' => 'required|exists:transaksi,id'
         ]);
 
+        $pesanan = Pesanan::with(['produk'])->where('id_transaksi', $request->id_transaksi)->get();
+        $pengirim = Auth::user();
+        $penerima = Transaksi::with('user', 'user.reseller_detail')->first()->user;
+
         $qrcode = QrcodeHelper::getQrcodeString($request->id_transaksi);
 
-        return view('invoices.nota', [ 
-            'qrcode' => $qrcode
+        return view('invoices.nota', [
+            'qrcode' => $qrcode,
+            'pesanan' => $pesanan,
+            'pengirim' => $pengirim,
+            'penerima' => $penerima,
         ]);
     }
+
 }
