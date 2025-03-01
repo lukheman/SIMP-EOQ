527c9df05b6f6c44fbbb751b2c4c804afc170a29
Akmal <akmalthemalong@gmail.com>
2025-03-01 11:15:17 +0800

    feat: tambah handler untuk laporan penjualan

--- app/Http/Controllers/AdminTokoController.php
+++ app/Http/Controllers/AdminTokoController.php
@@ -63,4 +63,23 @@ class AdminTokoController extends Controller
         ]);
     }
 
+    public function laporanPenjualan(Request $request) { 
+        $request->validate([
+            'periode' => 'required'
+        ]);
+
+        list($tahun, $bulan) = explode('-', $request->periode);
+
+        $penjualan = Penjualan::with('produk')
+            ->whereYear('tanggal', $tahun)
+            ->whereMonth('tanggal', $bulan)
+            ->get();
+
+        return view('invoices.laporan-penjualan', [
+            'penjualan' => $penjualan,
+            'periode' => $request->periode,
+        ]);
+
+    }
+
 }
