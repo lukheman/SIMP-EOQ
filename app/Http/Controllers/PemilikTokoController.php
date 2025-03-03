f0c1cedf0335fbdd60d480a0a0aecbbc016f6081
Akmal <akmalthemalong@gmail.com>
2025-03-04 07:58:46 +0800

    feat(controller): PemilikController

--- app/Http/Controllers/PemilikTokoController.php
+++ app/Http/Controllers/PemilikTokoController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mutasi;
 
 class PemilikTokoController extends Controller
 {
-    //
+
+    public function penjualan() {
+        $penjualan = Mutasi::where('jenis', 'keluar')->get();
+
+        return view('pemilik_toko.penjualan', [
+            'page' => 'Penjualan',
+            'penjualan' => $penjualan
+        ]);
+    }
+
+    public function laporanPenjualan(Request $request) {
+        $request->validate([
+            'periode' => 'required'
+        ]);
+
+        list($tahun, $bulan) = explode('-', $request->periode);
+
+        $penjualan = Mutasi::with('produk')
+            ->whereYear('tanggal', $tahun)
+            ->whereMonth('tanggal', $bulan)
+            ->where('jenis', 'keluar')
+            ->get();
+
+        return view('invoices.laporan-penjualan', [
+            'penjualan' => $penjualan,
+            'periode' => $request->periode,
+            'ttd' => 'Manager Toko'
+        ]);
+
+    }
 }
