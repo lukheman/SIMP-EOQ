11bc620d7e6338fbcf82ca4fc1350b9e870164d8
Akmal <akmalthemalong@gmail.com>
2025-03-08 09:23:05 +0800

    feat(controller): MutasiController.php

--- app/Http/Controllers/MutasiController.php
+++ app/Http/Controllers/MutasiController.php
@@ -9,13 +9,79 @@ use App\Models\Mutasi;
 class MutasiController extends Controller
 {
 
+    public function store(Request $request) {
+
+        $data = $request->validate([
+            'id_produk' => 'required|exists:produk,id',
+            'jumlah' => 'required',
+            'tanggal' => 'required',
+            'jenis' => 'required',
+        ]);
+
+        $mutasi =  Mutasi::create($data);
+
+        $message = $request->jenis == 'masuk' ? 'Berhasil menambahkan data barang masuk' : 'Berhasil menambahkan data barang keluar';
+
+        if($mutasi) {
+            return response()->json([
+                'success' => true,
+                'message' => $message,
+                'data' => $mutasi
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Gagal menambahkan data mutasi'
+        ], 500);
+
+    }
+
     public function destroy($id) {
         $penjualan = Mutasi::find($id)?->delete();
 
         return response()->json([
             'success' => true,
-            'message' => 'Log penjualan berhasil dihapus'
+            'message' => 'Log mutasi berhasil dihapus'
         ], 200);
+    }
+
+    public function show($id) {
+        $mutasi = Mutasi::with('produk')->find($id);
+
+        if($mutasi) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Data mutasi berhasil didapatkan.',
+                'data' => $mutasi
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Data mutasi gagal didapatkan.'
+        ], 500);
+    }
+
+    public function update(Request $request, $id) {
+
+        // validasi data
+        $data = $request->validate([
+            'id_produk' => 'required',
+            'jumlah' => 'required',
+            'tanggal' => 'required',
+        ]);
+
+        $mutasi = Mutasi::find($id);
+
+        $mutasi->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data mutasi berhasil diperbarui',
+            'data' => $mutasi
+        ], 200);
+
     }
 
 }
