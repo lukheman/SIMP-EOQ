cc3616d5074e05c58bd7368b7897365bd6712f51
Akmal <akmalthemalong@gmail.com>
2025-03-14 04:28:22 +0800

    feat(migrasi): perbarui skema tabel produk
    - Hapus constraint unik dari kolom 'nama_produk'
    - Tambahkan kolom 'biaya_penyimpanan', 'biaya_pemesanan', 'lead_time',
    'penggunaan_rata_rata', dan 'gambar'

--- database/migrations/2025_02_16_092346_create_produk_table.php
+++ database/migrations/2025_02_16_092346_create_produk_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('produk', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_produk', 100)->unique();
+            $table->string('nama_produk', 100);
             $table->string('kode_produk', 10)->unique();
             $table->integer('persediaan')->default(0);
             $table->decimal('harga_beli', 10, 2);
             $table->decimal('harga_jual', 10, 2);
+            $table->decimal('biaya_penyimpanan', 10, 2);
+            $table->decimal('biaya_pemesanan', 10, 2);
+            $table->integer('lead_time'); // waktu tunggu dalam satuan hari
+            $table->integer('penggunaan_rata_rata'); // harian
             $table->text('deskripsi')->nullable();
+            $table->string('gambar')->nullable();
             $table->timestamps();
         });
     }
