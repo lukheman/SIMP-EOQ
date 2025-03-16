52759d907bdd1a63c300fd43bf3cc2c6596a7c04
Akmal <akmalthemalong@gmail.com>
2025-03-16 10:45:24 +0800

    feat(kurir): update menu links and icons
    - Changed 'Dashboard' text to be wrapped in a <p> tag.
    - Updated 'Konfirmasi Pembayaran' link to 'Pesanan' with new icon and active state.
    - Added new 'Scan QR Code' link with appropriate icon and active state.

--- resources/views/kurir/menu.blade.php
+++ resources/views/kurir/menu.blade.php
@@ -1,14 +1,21 @@
 <li class="nav-item">
     <a href="{{ route('kurir.dashboard') }}" class="nav-link {{ $page === 'Dashboard' ? 'active' : '' }}">
         <i class="nav-icon fas fa-tachometer-alt"></i>
-        Dashboard
+        <p> Dashboard </p>
     </a>
 </li>
 
 <li class="nav-item">
-    <a href="{{ route('kurir.konfirmasi-pembayaran') }}"
+    <a href="{{ route('kurir.pesanan') }}" class="nav-link {{ $page === 'Pesanan' ? 'active' : '' }}">
+        <i class="nav-icon fas fa-clipboard-list"></i> 
+       <p>Pesanan</p> 
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="{{ route('kurir.scan-qrcode') }}"
         class="nav-link {{ $page === 'Konfirmasi Pembayaran' ? 'active' : '' }}">
-        <i class="nav-icon fas fa-tachometer-alt"></i>
-        Konfirmasi Pembayaran
+        <i class="nav-icon fas fa-qrcode"></i>
+       <p>Konfirmasi Pembayaran</p> 
     </a>
 </li>
