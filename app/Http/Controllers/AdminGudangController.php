b474190fa13d465234fbe98832434243d99ed476
Akmal <akmalthemalong@gmail.com>
2025-03-16 10:31:42 +0800

    feat: tambah metode perhitungan EOQ dan perbarui data produk

--- app/Http/Controllers/AdminGudangController.php
+++ app/Http/Controllers/AdminGudangController.php
@@ -49,28 +49,97 @@ class AdminGudangController extends Controller
     }
 
 
+    private function getPM($periode1, $periode2, $id_produk) {
+        $periode1 = new DateTime($periode1);
+        $periode2 = new DateTime($periode2);
+
+        $penjualanPeriode1 = Mutasi::where('id_produk', $id_produk)
+            ->whereYear('tanggal', $periode1->format('Y'))
+            ->whereMonth('tanggal', $periode1->format('m'))
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+        $penjualanPeriode2 = Mutasi::where('id_produk', $id_produk)
+            ->whereYear('tanggal', $periode2->format('Y'))
+            ->whereMonth('tanggal', $periode2->format('m'))
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+        return max($penjualanPeriode1, $penjualanPeriode2);
+
     }
 
-    public function eoq() {
-        $persediaan = Persediaan::with('produk')->get();
+    private function hitungSafetyStock($PM, $PRR, $LT) {
+        return ($PM - $PRR) * $LT;
+    }
+
+    private function hitungReorderPoint($SS, $LT, $Q) {
+        return $SS + ($LT * $Q);
+    }
+
+    private function hitungEconomicOrderQuantity($D, $S, $H) {
+        return sqrt((2 * $D * $S) / $H);
+    }
+
+
+    public function hitung(Request $request) {
+
+        $periodeAwal = $request->periode;
+        $periodeAkhir = (new DateTime($periodeAwal))->modify('+1 month')->format('Y-m');
+
+        $tanggalAwal = (new DateTime($periodeAwal))->modify('first day of this month')->format('Y-m-d');
+        $tanggalAkhir = (new DateTime($periodeAkhir))->modify('last day of this month')->format('Y-m-d');
+
+        $produk = Produk::all();
+
+        foreach($produk as $item) {
+
+            $D = Mutasi::where('id_produk', $item->id)
+                ->whereBetween('tanggal', [$tanggalAwal, $tanggalAkhir])
+                ->where('jenis', 'keluar')
+                ->sum('jumlah');
+
+            if($D <= 0) {
+                continue;
+            } 
+
+            $S = $item->biaya_pemesanan; // biaya pemesanan
+            $H = $item->biaya_penyimpanan; // biaya penyimpanan
+            $LT = $item->lead_time; // waktu tunggu
+            $Q = $item->rata_rata_penggunaan; // penggunaan rata-rata
+            $PRR = $D / 2; // penjualan rata-rata dibagi 2 karena dihitung dari 2 periode
+
+            $PM = $this->getPM($periodeAwal, $periodeAkhir, $item->id); // penjualan maksimum
+
+            $EOQ = $this->hitungEconomicOrderQuantity($D, $S, $H);
+
+            $SS = $this->hitungSafetyStock($PM, $PRR, $LT);
+
+            $ROP = $this->hitungReorderPoint($SS, $LT, $Q);
+
+            $item->eoq = round($EOQ);
+            $item->ss = $SS;
+            $item->rop = $ROP;
+            $item->pm = $PM;
+            $item->biaya_pemesanan = $S;
+            $item->biaya_penyimpanan = $H;
+
+        }
+
 
         return view('admin_gudang.eoq', [
             'page' => 'EOQ',
-            'persediaan' => $persediaan
+            'produk' => $produk
+        ]);
+
+    }
+
+    public function eoq() {
+
+        return view('admin_gudang.eoq', [
+            'page' => 'EOQ',
         ]);
     }
-
-    public function hitungEOQ(Request $request) {
-        $persediaan = Persediaan::all();
-
-        return response()->json([
-            'success' => true,
-            'message' => 'berhasil menghitung eoq',
-            // 'data' => $persediaan
-        ], 200);
-
-    }
-
 
     public function laporanPenjualan() {
         $penjualan = Mutasi::where('jenis', 'keluar')->get();
