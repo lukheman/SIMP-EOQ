35d41dabd844d62c71e3b2bfc215538faba30c6b
Akmal <akmalthemalong@gmail.com>
2025-03-18 21:47:53 +0800

    feat: migration pesanan

---
+++ database/migrations/2025_03_15_043406_create_pesanan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_produk')->constrained('produk')->onDelete('cascade');
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_transaksi')->nullable()->constrained('transaksi')->onDelete('cascade');
+            $table->foreignId('id_keranjang')->nullable()->constrained('keranjang')->onDelete('cascade');
+            $table->decimal('total_harga', 10, 2);
+            $table->integer('jumlah')->default(1);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanan');
+    }
+};
