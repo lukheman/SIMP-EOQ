50a4a031c05a3608efce2e6adc4698dcca73c749
Akmal <akmalthemalong@gmail.com>
2025-03-18 21:45:33 +0800

    refactor(seeder): perbarui data seeder mutasi dengan data baru

--- database/seeders/MutasiSeeder.php
+++ database/seeders/MutasiSeeder.php
@@ -15,55 +15,89 @@ class MutasiSeeder extends Seeder
     {
         date_default_timezone_set("Asia/Jakarta");
 
-        DB::table('mutasi')->insert([
-            [
-                'id_produk' => 1,
-                'tanggal' => date("Y-m-d"),
-                'jumlah' => 10,
+        DB::table('mutasi')->insert([ 
+            [ 
+                'id_produk' => 3,
+                'tanggal' => "2025-01-01",
+                'jumlah' => 50,
                 'jenis' => 'keluar'
             ],
-            [
-                'id_produk' => 2,
-                'tanggal' => date("Y-m-d"),
+            [ 
+                'id_produk' => 3,
+                'tanggal' => "2025-01-10",
+                'jumlah' => 50,
+                'jenis' => 'keluar'
+            ],
+            [ 
+                'id_produk' => 3,
+                'tanggal' => "2025-01-20",
                 'jumlah' => 20,
                 'jenis' => 'keluar'
             ],
-            [
+            [ 
                 'id_produk' => 3,
-                'tanggal' => date("Y-m-d"),
-                'jumlah' => 30,
-                'jenis' => 'masuk'
-            ],
-            [
-                'id_produk' => 4,
-                'tanggal' => date("Y-m-d"),
-                'jumlah' => 5,
-                'jenis' => 'masuk'
-            ],
-            [
-                'id_produk' => 6,
-                'tanggal' => "2024-10-01",
-                'jumlah' => 140,
-                'jenis' => 'masuk'
-            ],
-            [
-                'id_produk' => 6,
-                'tanggal' => "2024-10-01",
-                'jumlah' => 120,
+                'tanggal' => "2025-02-10",
+                'jumlah' => 65,
                 'jenis' => 'keluar'
             ],
-            [
-                'id_produk' => 6,
-                'tanggal' => "2024-11-01",
-                'jumlah' => 140,
-                'jenis' => 'masuk'
-            ],
-            [
-                'id_produk' => 6,
-                'tanggal' => "2024-11-01",
-                'jumlah' => 130,
+            [ 
+                'id_produk' => 3,
+                'tanggal' => "2025-02-10",
+                'jumlah' => 65,
                 'jenis' => 'keluar'
             ],
+
         ]);
+
+        /* DB::table('mutasi')->insert([ */
+        /*     [ */
+        /*         'id_produk' => 1, */
+        /*         'tanggal' => date("Y-m-d"), */
+        /*         'jumlah' => 10, */
+        /*         'jenis' => 'keluar' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 2, */
+        /*         'tanggal' => date("Y-m-d"), */
+        /*         'jumlah' => 20, */
+        /*         'jenis' => 'keluar' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 3, */
+        /*         'tanggal' => date("Y-m-d"), */
+        /*         'jumlah' => 30, */
+        /*         'jenis' => 'masuk' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 4, */
+        /*         'tanggal' => date("Y-m-d"), */
+        /*         'jumlah' => 5, */
+        /*         'jenis' => 'masuk' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 6, */
+        /*         'tanggal' => "2024-10-01", */
+        /*         'jumlah' => 140, */
+        /*         'jenis' => 'masuk' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 6, */
+        /*         'tanggal' => "2024-10-01", */
+        /*         'jumlah' => 120, */
+        /*         'jenis' => 'keluar' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 6, */
+        /*         'tanggal' => "2024-11-01", */
+        /*         'jumlah' => 140, */
+        /*         'jenis' => 'masuk' */
+        /*     ], */
+        /*     [ */
+        /*         'id_produk' => 6, */
+        /*         'tanggal' => "2024-11-01", */
+        /*         'jumlah' => 130, */
+        /*         'jenis' => 'keluar' */
+        /*     ], */
+        /* ]); */
     }
 }
