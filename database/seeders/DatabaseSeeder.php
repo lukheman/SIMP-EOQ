d3bd349e63c5c0d04cfbfc02e33dbdaa95e903d8
Akmal <akmalthemalong@gmail.com>
2025-04-13 18:41:58 +0800

    refactor(seeder): hapus PersediaanSeeder dari DatabaseSeeder
    - Menghapus pemanggilan `PersediaanSeeder` dari `DatabaseSeeder`.
    - Menghapus file `PersediaanSeeder.php` karena tidak lagi diperlukan.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProdukSeeder::class,
-            PersediaanSeeder::class,
             MutasiSeeder::class,
         ]);
     }
