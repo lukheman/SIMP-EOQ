4e1d29076aa0269993a96d7f293bfbedc95ebc9c
Akmal <akmalthemalong@gmail.com>
2025-04-28 21:52:43 +0800

    feat(status-transaksi): tambahkan status baru "DITERIMA" dan fungsi `values`
    - Menambahkan status transaksi baru dengan nama "DITERIMA".
    - Menambahkan fungsi statis `values` untuk mendapatkan daftar nilai dari semua kasus dalam enum `StatusTransaksi`.

--- app/Constants/StatusTransaksi.php
+++ app/Constants/StatusTransaksi.php
@@ -8,6 +8,11 @@ enum StatusTransaksi: string
     case DIPROSES = 'diproses';
     case DIKIRIM = 'dikirim';
     case DITOLAK = 'ditolak';
+    case DITERIMA = 'diterima';
     case SELESAI = 'selesai';
     case BATAL = 'batal';
+
+    public static function values(): array {
+        return array_map(fn($case) => $case->value , self::cases());
+    }
 }
