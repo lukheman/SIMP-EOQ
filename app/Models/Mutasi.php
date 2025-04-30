cfe9c49a9b9437a97b2cbc74b80988c2b5a3dbcc
Akmal <akmalthemalong@gmail.com>
2025-04-30 20:41:14 +0800

    feat(models): tambahkan metode untuk analisis penjualan di model Mutasi
    - Menambahkan metode `penjualanMaksimum` untuk menghitung penjualan maksimum antara dua periode tertentu.
    - Menambahkan metode `rataRataPenjualan` untuk menghitung rata-rata penjualan harian dalam satu bulan.
    - Menambahkan penggunaan `Carbon\Carbon` untuk manipulasi tanggal.
    
    Perubahan ini bertujuan untuk mendukung analisis data penjualan pada model Mutasi.

--- app/Models/Mutasi.php
+++ app/Models/Mutasi.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Mutasi extends Model
@@ -14,11 +16,50 @@ class Mutasi extends Model
         return $this->belongsTo(Produk::class, 'id_produk');
     }
 
-    public function getTotalHargaJualAttribute() { 
+    public function getTotalHargaJualAttribute() {
         return $this->produk->harga_jual * $this->jumlah;
     }
 
-    public function getTotalHargaBeliAttribute() { 
+    public function getTotalHargaBeliAttribute() {
         return $this->produk->harga_beli * $this->jumlah;
     }
+
+    public static function penjualanMaksimum($id_produk, $periode1, $periode2) {
+        /* $periode1 = Carbon::parse($periode1)->subMonth(); */
+        /* $periode2 = Carbon::parse($periode2)->subMonth(); */
+
+        $penjualanPeriode1 = self::where('id_produk', $id_produk)
+            ->whereYear('tanggal', $periode1->year)
+            ->whereMonth('tanggal', $periode1->month)
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+        $penjualanPeriode2 = self::where('id_produk', $id_produk)
+            ->whereYear('tanggal', $periode2->year)
+            ->whereMonth('tanggal', $periode2->month)
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+        return max($penjualanPeriode1, $penjualanPeriode2);
+
+    }
+
+    // rata-rata penjualan harian
+    public static function rataRataPenjualan($id_produk, Carbon $periode) {
+
+        /* $periode = Carbon::parse($periode); */
+
+        $penjualan = self::where('id_produk', $id_produk)
+            ->whereYear('tanggal', $periode->subMonth()->year)
+            ->whereMonth('tanggal', $periode->subMonth()->month)
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+        $rata_rata = $penjualan / $periode->daysInMonth;
+
+        return $rata_rata;
+
+    }
+
+
 }
