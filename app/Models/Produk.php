4c7e6ed70de55e283aa82cf803ba52dc0e2bcddf
Akmal <akmalthemalong@gmail.com>
2025-04-30 20:40:09 +0800

    feat(produk): tambahkan metode EOQ, safety stock, dan reorder point
    Menambahkan tiga metode baru pada model `Produk`:
    - `economicOrderQuantity`: Menghitung Economic Order Quantity (EOQ) berdasarkan data mutasi.
    - `safetyStock`: Menghitung safety stock berdasarkan penjualan maksimum, rata-rata, dan lead time.
    - `reorderPoint`: Menghitung reorder point berdasarkan safety stock, lead time, dan rata-rata penjualan.
    
    Perubahan ini bertujuan untuk mendukung perhitungan manajemen persediaan yang lebih akurat.

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Produk extends Model
 {
     protected $table = 'produk';
@@ -25,5 +27,58 @@ class Produk extends Model
     {
         return $this->persediaan >= $permintaan;
     }
+
+    public function economicOrderQuantity() {
+
+        $bulanAwal = Carbon::now()->subMonth(3);
+        $bulanAkhir = Carbon::now()->subMonth(2);
+
+        $tanggalAwal = $bulanAwal->startOfMonth()->toDateString();
+        $tanggalAkhir= $bulanAkhir->endOfMonth()->toDateString();
+
+        $D = Mutasi::where('id_produk', $this->id)
+            ->whereBetween('tanggal', [$tanggalAwal, $tanggalAkhir])
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+
+        $S = $this->biaya_pemesanan;
+        $H = $this->biaya_penyimpanan;
+
+
+        return sqrt((2 * $D * $S) / $H);
+
+    }
+
+
+    public function safetyStock() {
+
+        $bulanAwal = Carbon::now()->subMonth(3);
+        $bulanAkhir = Carbon::now()->subMonth(2);
+
+        $tanggalAwal = $bulanAwal->startOfMonth()->toDateString();
+        $tanggalAkhir= $bulanAkhir->endOfMonth()->toDateString();
+
+        $D = Mutasi::where('id_produk', $this->id)
+            ->whereBetween('tanggal', [$tanggalAwal, $tanggalAkhir])
+            ->where('jenis', 'keluar')
+            ->sum('jumlah');
+
+        $PM = Mutasi::penjualanMaksimum($this->id, $bulanAwal, $bulanAkhir); // penjualan maksimum
+        $PRR = $D / 2; // penjualan rata-rata dibagi 2 karena dihitung dari 2 periode
+        $LT = $this->lead_time; // waktu tunggu
+
+        return ($PM - $PRR) * $LT;
+    }
+
+    public function reorderPoint() {
+
+        $SS = $this->safetyStock();
+        $LT = $this->lead_time; // waktu tunggu
+        $Q = Mutasi::rataRataPenjualan($this->id, Carbon::now());
+        return $SS + ($LT * $Q);
+    }
+
+
 
 }
