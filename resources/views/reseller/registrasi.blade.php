e2ec5461bd38ca1fadde88ec77ed45439f7cd962
Akmal <akmalthemalong@gmail.com>
2025-05-04 20:09:47 +0800

    feat(reseller): tambahkan halaman registrasi reseller
    Menambahkan file baru `registrasi.blade.php` untuk halaman registrasi reseller. Halaman ini mencakup form pendaftaran dengan input untuk nama lengkap, email, password, dan konfirmasi password. Juga disertakan integrasi dengan AdminLTE untuk tampilan antarmuka.

---
+++ resources/views/reseller/registrasi.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
+
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+        <!-- Font Awesome Icons -->
+        <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css')}}">
+
+    </head>
+
+    <body class="register-page">
+        <div class="register-box">
+            <div class="register-logo">
+                <a href=""><b>Registrasi</b></a>
+            </div>
+
+            <div class="card">
+                <div class="card-body register-card-body">
+                    <p class="login-box-msg">Daftar menjadi reseller</p>
+
+
+                    <form action="{{ route('reseller.signup') }}"  method="post">
+                    @csrf
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Full name" name="name">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="email" class="form-control" placeholder="Email" name="email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" name="password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Retype password" name="confirm_password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8">
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block">Register</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+
+                </div>
+                <!-- /.form-box -->
+            </div><!-- /.card -->
+        </div>
+
+        <!-- REQUIRED SCRIPTS -->
+
+        <!-- jQuery -->
+        <script src="{{ asset('plugins/jquery/jquery.min.js')}}"></script>
+        <!-- Bootstrap 4 -->
+        <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <!-- AdminLTE App -->
+        <script src="{{ asset('dist/js/adminlte.min.js')}}"></script>
+
+
+    </body>
+
+</html>
+
+@section('content')
