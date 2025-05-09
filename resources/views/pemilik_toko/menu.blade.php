f1aaad4eab87238af56d7c9ed110848a4693bc5f
Akmal <akmalthemalong@gmail.com>
2025-05-09 11:27:42 +0800

    refactor(view): ubah elemen navigasi menjadi komponen blade kustom
    Mengganti elemen `<li>` dan `<a>` pada menu navigasi dengan komponen Blade `<x-nav-link>` untuk meningkatkan keterbacaan dan konsistensi kode.

--- resources/views/pemilik_toko/menu.blade.php
+++ resources/views/pemilik_toko/menu.blade.php
@@ -1,31 +1,30 @@
-<li class="nav-item">
-    <a href="{{ route('pemiliktoko.dashboard') }}" class="nav-link {{ $page === 'Dashboard' ? 'active' : '' }}">
-        <i class="nav-icon fas fa-tachometer-alt"></i>
-        <p> Dashboard </p>
-    </a>
-</li>
+<x-nav-link
+    href="{{ route('pemiliktoko.dashboard') }}"
+    :active="$page === 'Dashboard'"
+    icon="fa-tachometer-alt">
+    Dashboard
+</x-nav-link>
+
 
 <li class="nav-header">LAPORAN</li>
 
-<li class="nav-item">
-    <a href="{{ route('pemiliktoko.laporan-penjualan') }}"
-        class="nav-link  {{ $page === 'Laporan Penjualan' ? 'active' : '' }}"> <i class="fas fa-clipboard-list nav-icon"></i>
-        <p>Penjualan</p>
-    </a>
-</li>
+<x-nav-link
+    href="{{ route('pemiliktoko.laporan-penjualan') }}"
+    :active="$page === 'Laporan Penjualan'"
+    icon="fa-clipboard-list">
+    Penjualan
+</x-nav-link>
 
-<li class="nav-item">
-    <a href="{{ route('pemiliktoko.laporan-persediaan-produk') }}" class="nav-link {{
-        $page==='Laporan Persediaan Produk' ? 'active' : '' }} "> 
-        <i class="fas fa-boxes nav-icon"></i>
-        <p>Persediaan Produk</p>
-    </a>
-</li>
+<x-nav-link
+    href="{{ route('pemiliktoko.laporan-persediaan-produk') }}"
+    :active="$page === 'Laporan Persediaan Produk'"
+    icon="fa-boxes">
+    Persediaan Produk
+</x-nav-link>
 
-<li class="nav-item">
-    <a href="{{ route('pemiliktoko.laporan-barang-masuk') }}"
-        class="nav-link  {{ $page === 'Laporan Barang Masuk' ? 'active' : '' }}">
-        <i class="fas fa-box-open nav-icon"></i>
-        <p>Barang Masuk</p>
-    </a>
-</li>
+<x-nav-link
+    href="{{ route('pemiliktoko.laporan-barang-masuk') }}"
+    :active="$page === 'Laporan Barang Masuk'"
+    icon="fa-box-open">
+    Barang Masuk
+</x-nav-link>
