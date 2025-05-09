8f1f17ff278cd3b710fc4f111646c9270cddedba
Akmal <akmalthemalong@gmail.com>
2025-05-09 22:22:02 +0800

    refactor(seeder): memperbarui struktur data dan logika UserSeeder
    - Mengubah format array roles untuk menyelaraskan nama dan role.
    - Memperbaiki logika iterasi pada foreach untuk mencocokkan key dan value.
    - Mengubah format email agar sesuai dengan nama yang diubah menjadi lowercase dan tanpa spasi.
    - Menghapus penyisipan data ke tabel 'reseller_detail' karena tidak relevan dengan UserSeeder.
    
    Perubahan ini bertujuan untuk meningkatkan konsistensi dan keterbacaan kode.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,19 +16,20 @@ class UserSeeder extends Seeder
     {
         // Daftar role dengan nama berbeda untuk setiap role
         $roles = [
-            'reseller' => 'Marcus',
-            'reseller' => 'Citra',
-            'admin_toko' => 'Nuruddin',
-            'admin_gudang' => 'Madun',
-            'pemilik_toko' => 'Hendri',
-            'kurir' => 'Ihwan',
+            'Reseller 1' => 'reseller',
+            'Reseller 2' => 'reseller',
+            'admin toko' => 'admintoko',
+            'admin gudang' => 'admingudang',
+            'pemilik toko' => 'pemiliktoko',
+            'Kurir 1' => 'kurir',
+            'Kurir 2' => 'kurir',
         ];
 
         // Menyisipkan data ke tabel 'users'
-        foreach ($roles as $role => $name) {
+        foreach ($roles as $name => $role) {
             DB::table('users')->insert([
                 /* 'username' => $role, */
-                'email' => $role . '@example.com',
+                'email' => str_replace(' ', '', strtolower($name)) . '@example.com',
                 'password' => Hash::make('password123'), // Gunakan hashing untuk keamanan
                 'role' => $role,
                 'name' => $name, // Nama sesuai dengan role
@@ -37,11 +38,5 @@ class UserSeeder extends Seeder
                 'updated_at' => now(),
             ]);
         }
-
-        // Menyisipkan data ke tabel 'reseller_detail' untuk id_user 1
-        DB::table('reseller_detail')->insert([
-            'id_user' => 1,
-            'alamat' => 'Depan gerbang utama kampus USN'
-        ]);
     }
 }
