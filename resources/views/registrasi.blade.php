cb6a9a420b03bc544baa4dec0ec1429efa8ecef5
Akmal <akmalthemalong@gmail.com>
2025-05-09 22:17:33 +0800

    feat: memperbarui form registrasi dan mengubah struktur file
    - Memindahkan file `registrasi.blade.php` dari folder `reseller` ke folder utama `views`.
    - Menambahkan opsi pendaftaran sebagai "Reseller" atau "Kurir" menggunakan dropdown.
    - Menambahkan input untuk nomor telepon.
    - Mengubah placeholder beberapa input agar lebih sesuai dengan bahasa Indonesia.
    - Mengganti nama field `confirm_password` menjadi `password_confirmation`.
    - Mengubah teks tombol dari "Register" menjadi "Daftar".

--- resources/views/registrasi.blade.php
+++ resources/views/registrasi.blade.php
@@ -28,19 +28,37 @@
 
             <div class="card">
                 <div class="card-body register-card-body">
-                    <p class="login-box-msg">Daftar menjadi reseller</p>
 
-
-                    <form action="{{ route('reseller.signup') }}"  method="post">
+                    <form action="{{ route('signup') }}"  method="post">
                     @csrf
+
+                        <div class="form-group">
+                            <label for="role">Daftar Sebagai</label>
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Full name" name="name">
+                            <!-- <input type="text" class="form-control" placeholder="Nama Anda" name="name"> -->
+                            <select name="role" id="role" class="form-control">
+                                <option value="{{ \App\Constants\Role::RESELLER}}">Reseller</option>
+                                <option value="{{ \App\Constants\Role::KURIR}}">Kurir</option>
+                            </select>
+
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-vector-square"></span>
+                                </div>
+                            </div>
+
+                        </div>
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Nama Anda" name="name">
                             <div class="input-group-append">
                                 <div class="input-group-text">
                                     <span class="fas fa-user"></span>
                                 </div>
                             </div>
                         </div>
+
                         <div class="input-group mb-3">
                             <input type="email" class="form-control" placeholder="Email" name="email">
                             <div class="input-group-append">
@@ -49,6 +67,16 @@
                                 </div>
                             </div>
                         </div>
+
+                        <div class="input-group mb-3">
+                            <input type="phone" class="form-control" placeholder="Nomor Telephone" name="phone">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-phone"></span>
+                                </div>
+                            </div>
+                        </div>
+
                         <div class="input-group mb-3">
                             <input type="password" class="form-control" placeholder="Password" name="password">
                             <div class="input-group-append">
@@ -58,7 +86,7 @@
                             </div>
                         </div>
                         <div class="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Retype password" name="confirm_password">
+                            <input type="password" class="form-control" placeholder="Ulangi Password" name="password_confirmation">
                             <div class="input-group-append">
                                 <div class="input-group-text">
                                     <span class="fas fa-lock"></span>
@@ -70,7 +98,7 @@
                             </div>
                             <!-- /.col -->
                             <div class="col-4">
-                                <button type="submit" class="btn btn-primary btn-block">Register</button>
+                                <button type="submit" class="btn btn-primary btn-block">Daftar</button>
                             </div>
                             <!-- /.col -->
                         </div>
