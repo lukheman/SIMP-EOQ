1c62954a6810602c76034e3e48ee3a74ca2b1da8
Akmal <akmalthemalong@gmail.com>
2025-05-13 13:15:07 +0800

    feat(admin-toko): tambahkan fitur transaksi dan pengurangan persediaan
    Menambahkan metode `transaksi` untuk memproses transaksi baru, termasuk:
    - Pembuatan transaksi dengan metode pembayaran default `TUNAI` dan status `SELESAI`.
    - Validasi persediaan produk sebelum membuat pesanan.
    - Pembuatan pesanan berdasarkan produk yang dipesan.
    - Mengurangi persediaan produk setelah transaksi berhasil.
    
    Juga menambahkan metode `kurangiPersediaan` untuk mengelola pengurangan persediaan produk dan mencatat log mutasi.
    
    Selain itu, menambahkan metode `kasir` untuk menampilkan halaman kasir.

--- app/Http/Controllers/AdminTokoController.php
+++ app/Http/Controllers/AdminTokoController.php
@@ -15,11 +15,96 @@ use App\Models\Produk;
 
 use App\Constants\StatusTransaksi;
 use App\Constants\Role;
+use App\Constants\MetodePembayaran;
 
 
 
 class AdminTokoController extends Controller
 {
+
+    public function transaksi(Request $request) {
+
+
+        $transaksi = Transaksi::create([
+            'metode_pembayaran' => MetodePembayaran::TUNAI,
+            'status' => StatusTransaksi::SELESAI
+        ]);
+
+        foreach($request->pesanan as $barcode => $value) {
+
+            $produk = Produk::query()->where('kode_produk', $barcode)->first();
+
+            // cek persediaan produk
+            if(!$produk->isPersediaanMencukupi($value['jumlah'])) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $produk->nama_produk . ' tidak cukup di persediaan'
+                ], 200);
+            }
+
+            // kalkulasi total harga barang yang dipesan
+            $total_harga = $produk->harga_jual * $value['jumlah'];
+
+            // buat pesanan
+            $pesanan = Pesanan::create([
+                'id_produk' => $produk->id,
+                'jumlah' => $value['jumlah'],
+                'total_harga' => $total_harga,
+                'id_transaksi' => $transaksi->id
+            ]);
+
+        }
+
+        $this->kurangiPersediaan($transaksi);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaksi berhasil dilakukan',
+            'transaksi' => $transaksi
+        ], 200);
+
+    }
+
+    private function kurangiPersediaan(Transaksi $transaksi) {
+        $pesanan = Pesanan::with(['produk', 'produk.persediaan'])->where('id_transaksi', $transaksi->id)->get();
+
+        foreach ($pesanan as $item) {
+
+            if(!$item->produk->isPersediaanMencukupi($item->jumlah)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "Persediaan {$item->produk->nama_produk} tidak mencukupi",
+                ], 200);
+            }
+
+            // kurangi persediaan produk
+            $item->produk->persediaan->jumlah -= $item->jumlah;
+            $item->produk->persediaan->save();
+
+            // catat log mutasi
+            Mutasi::create([
+                'id_produk' => $item->produk->id,
+                'jumlah' => $item->jumlah,
+                'jenis' => 'keluar',
+                'keterangan' => 'Pembelian langsung'
+            ]);
+
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Persediaan pesanan berhasil dikurangi',
+        ], 200);
+    }
+
+
+    public function kasir() {
+
+        return view('admin_toko.kasir', [
+            'page' => 'Kasir'
+        ]);
+
+    }
 
     public function index() {
         $pesanan = Transaksi::where('status', StatusTransaksi::PENDING)->count();
