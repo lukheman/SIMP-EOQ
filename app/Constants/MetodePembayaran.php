8d405f3ff0dd455c05b7a7d5bdd3b4004b3ff893
Akmal <akmalthemalong@gmail.com>
2025-05-13 13:14:48 +0800

    feat: tambahkan metode pembayaran baru "tunai"
    Menambahkan opsi metode pembayaran baru dengan nama "tunai" pada enum `MetodePembayaran`. Perubahan ini juga memperbaiki format kode pada fungsi `values`.

--- app/Constants/MetodePembayaran.php
+++ app/Constants/MetodePembayaran.php
@@ -6,8 +6,9 @@ namespace App\Constants;
 enum MetodePembayaran: string {
     case COD = 'cod';
     case TRANSFER = 'transfer';
+    case TUNAI = 'tunai';
 
-    public static function values(): array { 
+    public static function values(): array {
         return array_map(fn($case) => $case->value, self::cases());
     }
 
