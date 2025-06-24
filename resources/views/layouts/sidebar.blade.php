5591fa149ce6076cf9a5d731a43a5ef05891054e
Akmal <akmalthemalong@gmail.com>
2025-06-24 18:48:24 +0800

    feat(layouts/sidebar): ganti logo gambar dengan ikon font dan buat nama brand dinamis
    Sidebar kini menggunakan ikon font sebagai logo dan nama brand diambil dari konfigurasi aplikasi atau default ke 'UD Toko Diva'.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="{{ asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">SIMP</span>
+        <i class="fas fa-home brand-image elevation-3" style="opacity: .8; font-size: 24px; margin-left: 10px;"></i>
+        <span class="brand-text font-weight-light">{{ config('app.name') ?? 'UD Toko Diva'}}</span>
     </a>
 
     <!-- Sidebar -->
