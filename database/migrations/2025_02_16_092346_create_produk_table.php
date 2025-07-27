1a87447ccf1e25df1d4d11b666f010da88817898
Akmal <akmalthemalong@gmail.com>
2025-07-27 16:20:12 +0800

    feat(produk): Menambahkan kolom harga jual per unit dan jumlah per bal

--- database/migrations/2025_02_16_092346_create_produk_table.php
+++ database/migrations/2025_02_16_092346_create_produk_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('nama_produk', 100);
             $table->string('kode_produk', 20)->unique();
-            $table->decimal('harga_beli', 10, 2);
-            $table->decimal('harga_jual', 10, 2);
+            $table->decimal('harga_beli', 10, 2); // perbal
+            $table->decimal('harga_jual', 10, 2); // perbal
             $table->integer('lead_time')->default(0); // waktu tunggu dalam satuan hari
             $table->text('deskripsi')->nullable();
             $table->string('gambar')->nullable();
             $table->date('exp')->nullable();
+            $table->decimal('harga_jual_pcs', 10, 2);
+            $table->integer('pcs_per_bal')->default(0);
             $table->timestamps();
         });
     }
