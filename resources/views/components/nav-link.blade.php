392e67b87f81f1f3c01904b960dbbeae7be22dcc
Akmal <akmalthemalong@gmail.com>
2025-07-28 15:52:00 +0800

    fix: Pastikan tautan navigasi aktif berdasarkan URL penuh

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,6 @@
 <div>
     <li class="nav-item">
-        <a href="{{ $href }}" class="nav-link {{ $active ? 'active' : '' }}">
+        <a href="{{ $href }}" class="nav-link {{ $active ? 'active' : '' }} {{ request()->fullUrl() === $href ? 'active' : ''}}">
             <i class="nav-icon fas {{ $icon }}"></i>
             <p>{{ $slot }}</p>
         </a>
