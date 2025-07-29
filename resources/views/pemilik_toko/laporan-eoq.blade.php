c5effd93c7105c4fd5fa3e9b25826887143de776
Akmal <akmalthemalong@gmail.com>
2025-07-29 16:46:04 +0800

    feat: Tingkatkan rincian data laporan EOQ

--- resources/views/pemilik_toko/laporan-eoq.blade.php
+++ resources/views/pemilik_toko/laporan-eoq.blade.php
@@ -9,73 +9,60 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-
-            <div class="card-header">
+<div class="card">
+    <div class="card-header">
 
                 <button class="btn btn-outline-danger" data-toggle="modal" data-target="#modal-cetak-laporan-eoq">
                     <i class="fas fa-print"></i>
                     Cetak Laporan EOQ</button>
+    </div>
+    <div class="card-body">
+        <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+            <div class="row">
+                <div class="col-sm-12">
+                    <table id="datatable" class="table table-bordered table-striped dataTable dtr-inline"
+                        aria-describedby="datatable_info">
+                        <thead>
+                            <tr class="text-center">
+                                <th>Kode Produk</th>
+                                <th>Nama Produk</th>
+                                <th>Safety Stock</th>
+                                <th>ROP</th>
+                                <th>Persediaan Saat Ini</th>
+                                <th>Jumlah yang harus dipesan (EOQ)</th>
+                                <th>Frekuensi Pemesanan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
+                            @foreach($produk as $item)
+                            <tr>
+                                <td class="text-center">{{ $item->kode_produk }}</td>
+                                <td>{{ $item->nama_produk }}</td>
+                                <td class="text-center">{{ $item->safety_stock }}</td>
+                                <td class="text-center">{{ $item->reorder_point }}</td>
+                                <td class="text-center">{{ $item->label_persediaan }}</td>
+                                <td class="text-center">{{ $item->economic_order_quantity }}</td>
+                                <td class="text-center">{{ $item->frekuensi_pemesanan}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+
+                        </tfoot>
+                    </table>
+                </div>
             </div>
-            <div class="card-body">
-
-
-                <div id="datatable_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <table id="datatable" class="table table-bordered table-striped dataTable dtr-inline" >
-                                <thead>
-                                    <tr class="text-center">
-                                        <th>Tanggal</th>
-                                        <th>Nama Produk </th>
-                                        <th>Safety Stock</th>
-                                        <th>ROP</th>
-                                        <th>Jumlah harus dipesan (EOQ)</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                    @foreach ($data_eoq as $item)
-                                    <tr>
-                                        @if (count($item) < 3)
-                                            <td> {{ $item['periode']}}</td>
-                                            <td> {{ $item['nama_produk']}}</td>
-                                            <td colspan="3" class="text-danger text-center">Data penjualan tidak mencukupi</td>
-                                        @else
-
-                                        <td> {{ $item['periode']}}</td>
-                                        <td> {{ $item['nama_produk']}}</td>
-                                        <td> {{ round($item['safety_stock'])}}</td>
-                                        <td> {{ round($item['reorder_point'])}}</td>
-                                        <td> {{ round($item['eoq'])}}</td>
-
-                                        @endif
-                                    </tr>
-                                    @endforeach
-
-                                </tbody>
-                                <tfoot>
-
-                                </tfoot>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-12 col-md-7">
-                            <div class="dataTables_paginate paging_simple_numbers" id="datatable_paginate">
-                                <ul class="pagination">
-                                </ul>
-                            </div>
-                        </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-5">
+                </div>
+                <div class="col-sm-12 col-md-7">
+                    <div class="dataTables_paginate paging_simple_numbers" id="datatable_paginate">
+                        <ul class="pagination">
+                        </ul>
                     </div>
                 </div>
-
-
             </div>
-
         </div>
     </div>
 </div>
